6f3161ded0750286a42939eb5aa367687458d22f
Mats Hagen <matshagen@hotmail.com>
2019-07-19 16:54:52 +0200

    Added info on weather search

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longtitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/773fbd0672c0fa0ca7b048b238e9c163/' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longtitude) + '?units=si'
+    const url = 'https://api.darksky.net/forecast/773fbd0672c0fa0ca7b048b238e9c163/' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longtitude) + '?units=si&lang=nb'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -9,7 +9,7 @@ const forecast = (latitude, longtitude, callback) => {
         } else if (body.error) {
             callback('Unable to find weather for location, try new location!')
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' Det er ' + body.currently.temperature + ' grader nå (i dag: høyeste temperatur - ' + body.daily.data[0].temperatureHigh + ' og laveste temperatur - ' + body.daily.data[0].temperatureLow + ' grader). Det er ' + body.currently.precipProbability * 100 + '% sannsynlighet for regn nå. UV indeks: ' + body.daily.data[0].uvIndex)
         }
     })
 }
